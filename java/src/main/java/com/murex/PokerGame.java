5154d726912cace61f6d534472e3baa56331e721
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-11-22 15:56:45 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 40
        test: 0
    test-stats:
        run: 12
        passed: 12
        failed: 0
        skipped: 0
        error: 0
        duration: 34ms

--- java/src/main/java/com/murex/PokerGame.java
+++ java/src/main/java/com/murex/PokerGame.java
@@ -51,44 +51,4 @@ class PokerGame {
         return null;
     }
 
-    private String checkPair() {
-        Optional<Card> blackPairCard = getCardOfPairs(blackHand);
-        Optional<Card> whitePairCard = getCardOfPairs(whiteHand);
-
-        if(blackPairCard.isPresent() && whitePairCard.isPresent()) {
-            int comparison = blackPairCard.get().compareTo(whitePairCard.get());
-            if (comparison > 0) {
-                return buildPairCardsMessage(blackHand, blackPairCard.get());
-            } else if (comparison < 0) {
-                return buildPairCardsMessage(whiteHand, whitePairCard.get());
-            }else {
-                int highCardComparison = blackHand.getCards()[4].compareTo(whiteHand.getCards()[4]);
-                if (highCardComparison > 0) {
-                    return blackHand.getName() + " wins. - with Pair cards and higher rank: " + blackPairCard.get().getValue() + " and " + blackHand.getCards()[4].getValue();
-                }
-                if (highCardComparison < 0) {
-                    return whiteHand.getName() + " wins. - with Pair cards and higher rank: " + whitePairCard.get().getValue() + " and " + whiteHand.getCards()[4].getValue();
-                }
-            }
-        }
-
-        if(blackPairCard.isPresent()) {
-            return buildPairCardsMessage(blackHand, blackPairCard.get());
-        }
-
-        if(whitePairCard.isPresent()) {
-            return buildPairCardsMessage(whiteHand, whitePairCard.get());
-        }
-
-        return null;
-    }
-
-    private static String buildPairCardsMessage(Hand hand, Card card) {
-        return hand.getName() + " wins. - with Pair cards: " + card.getValue();
-    }
-
-    private Optional<Card> getCardOfPairs(Hand pokerHand) {
-        Map<Card, Long> cardsPairMap = Arrays.stream(pokerHand.getCards()).collect(groupingBy(Function.identity(), counting()));
-        return cardsPairMap.keySet().stream().filter(card -> cardsPairMap.get(card) == 2).findAny();
-    }
 }
