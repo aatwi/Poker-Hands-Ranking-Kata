12c0984dd0ecac2f010c8718dd27684671726102
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-11-22 16:38:53 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 7
        test: 0
    test-stats:
        run: 12
        passed: 12
        failed: 0
        skipped: 0
        error: 0
        duration: 56ms

--- java/src/main/java/com/murex/Result.java
+++ java/src/main/java/com/murex/Result.java
@@ -1,4 +1,11 @@
 package com.murex;
 
 public record Result(boolean isMatch, String message) {
+
+    public static Result aMatchResult(String message) {
+        return new Result(true, message);
+    }
+    public static Result aNoMatchResult() {
+        return new Result(false, "");
+    }
 }
