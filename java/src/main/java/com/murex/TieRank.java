09cd081990dc0b74776c9be2e1117325253260b9
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-11-22 15:34:29 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 4
        test: 0
    test-stats:
        run: 12
        passed: 12
        failed: 0
        skipped: 0
        error: 0
        duration: 44ms

--- java/src/main/java/com/murex/TieRank.java
+++ java/src/main/java/com/murex/TieRank.java
@@ -1,9 +1,5 @@
 package com.murex;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-
 public class TieRank extends PokerHandRank {
     public TieRank(Hand blackHand, Hand whiteHand) {
         super(blackHand, whiteHand);
@@ -13,8 +9,9 @@ public class TieRank extends PokerHandRank {
     public String verify() {
         Card[] blackCards = this.blackHand.getCards();
         Card[] whiteCards = this.blackHand.getCards();
+
         for (int index = 0; index < blackCards.length; index++) {
-            if(blackCards[index].compareTo(whiteCards[index])!= 0) {
+            if (blackCards[index].compareTo(whiteCards[index]) != 0) {
                 return null;
             }
         }
