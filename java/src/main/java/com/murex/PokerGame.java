8f057d8e4e05d27bdb9ba4f1f928c9958ab7d12c
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-11-22 16:24:06 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 3
        test: 0
    test-stats:
        run: 12
        passed: 12
        failed: 0
        skipped: 0
        error: 0
        duration: 39ms

--- java/src/main/java/com/murex/PokerGame.java
+++ java/src/main/java/com/murex/PokerGame.java
@@ -28,7 +28,7 @@ import java.util.List;
 class PokerGame {
     private final Hand blackHand;
     private final Hand whiteHand;
-    private List<PokerHandRank> ranks;
+    private List<PokerHandRank> ranks = new ArrayList<>();
 
     public PokerGame(String blackCards, String whiteCards) {
         this.blackHand = Hand.buildFrom("Black", blackCards);
@@ -36,7 +36,6 @@ class PokerGame {
     }
 
     public String getWinner() {
-        ranks = new ArrayList<>();
         ranks.add(new PairCardRank(blackHand, whiteHand));
         ranks.add(new HighCardRank(blackHand, whiteHand));
         ranks.add(new TieRank(blackHand, whiteHand));
