df1aca8865970e00e33adcb7bd01f90c88b75fc3
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-11-30 18:23:01 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 15
    test-stats:
        run: 24
        passed: 24
        failed: 0
        skipped: 0
        error: 0
        duration: 160ms

--- java/src/test/java/com/murex/ranking/PairCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/PairCardRankingTest.java
@@ -1,7 +1,6 @@
 package com.murex.ranking;
 
 import com.murex.Hand;
-import com.murex.Result;
 import org.junit.jupiter.api.Test;
 
 import static com.murex.Result.aMatchResult;
@@ -33,10 +32,10 @@ class PairCardRankingTest {
     @Test
     public void
     PAIR_black_wins_with_ace_as_pair() {
-        assertMatchingResult("7H JC KD AH AS", "2D 3H 5C 9S KH", aMatchResult("Black wins. - with Pair cards: Ace"), "Black wins. - with Pair cards: Ace");
+        assertMatchingResult("7H JC KD AH AS", "2D 3H 5C 9S KH", "Black wins. - with Pair cards: Ace");
     }
 
-    private static void assertMatchingResult(String cards, String cards1, Result expected, String message) {
+    private static void assertMatchingResult(String cards, String cards1, String message) {
         Hand blackHand = Hand.buildFrom("Black", cards);
         Hand whiteHand = Hand.buildFrom("White", cards1);
         PairCardRanking pairCardRanking = new PairCardRanking(blackHand, whiteHand);
@@ -47,31 +46,31 @@ class PairCardRankingTest {
     @Test
     public void
     PAIR_black_wins_with_jack_as_pair() {
-        assertMatchingResult("7H JH JC KD AS", "2D 3H 5C 9S KH", aMatchResult("Black wins. - with Pair cards: Jack"), "Black wins. - with Pair cards: Jack");
+        assertMatchingResult("7H JH JC KD AS", "2D 3H 5C 9S KH", "Black wins. - with Pair cards: Jack");
     }
 
     @Test
     public void
     PAIR_white_wins_with_4_as_pair() {
-        assertMatchingResult("5D 6C 7H JH AS", "2D 4H 4C 9S KH", aMatchResult("White wins. - with Pair cards: 4"),  "White wins. - with Pair cards: 4" );
+        assertMatchingResult("5D 6C 7H JH AS", "2D 4H 4C 9S KH", "White wins. - with Pair cards: 4" );
     }
 
     @Test
     public void
     PAIR_white_wins_with_ace_over_a_pair_of_2() {
-        assertMatchingResult("2H 2D 5S 6C AS", "4D 9S KH AH AC", aMatchResult("White wins. - with Pair cards: Ace"), "White wins. - with Pair cards: Ace");
+        assertMatchingResult("2H 2D 5S 6C AS", "4D 9S KH AH AC", "White wins. - with Pair cards: Ace");
     }
 
     @Test
     public void
     PAIR_white_wins_with_ace_over_a_pair_of_ace_due_to_other_rank() {
-        assertMatchingResult("2H 2D 5S 6C JS", "2C 2S 6H 8H AC", aMatchResult("White wins. - with Pair cards and higher rank: 2 and Ace"),  "White wins. - with Pair cards and higher rank: 2 and Ace" );
+        assertMatchingResult("2H 2D 5S 6C JS", "2C 2S 6H 8H AC", "White wins. - with Pair cards and higher rank: 2 and Ace" );
     }
 
     @Test
     public void
     PAIR_black_wins_with_ace_over_a_pair_of_ace_due_to_other_rank() {
-        assertMatchingResult("2H 2D 5S 6C KS", "2C 2S 6H 8H JC", aMatchResult("Black wins. - with Pair cards and higher rank: 2 and King"),  "Black wins. - with Pair cards and higher rank: 2 and King" );
+        assertMatchingResult("2H 2D 5S 6C KS", "2C 2S 6H 8H JC", "Black wins. - with Pair cards and higher rank: 2 and King" );
     }
 
 }
