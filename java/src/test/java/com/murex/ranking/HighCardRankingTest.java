230222e104f1645d729952f4da27f2319519784a
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-12-01 16:24:34 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 10
    test-stats:
        run: 28
        passed: 28
        failed: 0
        skipped: 0
        error: 0
        duration: 70ms

--- java/src/test/java/com/murex/ranking/HighCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/HighCardRankingTest.java
@@ -24,23 +24,23 @@ class HighCardRankingTest {
     @Test
     public void
     HIGH_CARD_black_wins_with_Queen(){
-        assertMatchingResult("Black wins. - with high card: Queen", "2H 3D 5S 9C QD", "2C 3H 4S 8C JH");
+        assertMatchingResult("2H 3D 5S 9C QD", "2C 3H 4S 8C JH", "Black wins. - with high card: Queen");
     }
 
     @Test
     public void
     HIGH_CARD_white_wins_Jack(){
-        assertMatchingResult("White wins. - with high card: Jack", "2H 3D 5S 7C 9D", "2C 3H 4S 8C JH");
+        assertMatchingResult("2H 3D 5S 7C 9D", "2C 3H 4S 8C JH", "White wins. - with high card: Jack");
     }
 
     @Test
     public void
     HIGH_CARD_black_wins_with_9(){
-        assertMatchingResult("Black wins. - with high card: 9", "2H 3D 5S 8C 9D", "2C 3H 4S 7C 8H");
+        assertMatchingResult("2H 3D 5S 8C 9D", "2C 3H 4S 7C 8H", "Black wins. - with high card: 9");
     }
 
-    private static void assertMatchingResult(String expected, String black, String white) {
-        HighCardRanking highCardRanking = buildHighCardRanking(black, white);
+    private static void assertMatchingResult(String blackCards, String whiteCards, String expected) {
+        HighCardRanking highCardRanking = buildHighCardRanking(blackCards, whiteCards);
 
         assertEquals(expected, highCardRanking.getMatchingResult().message());
     }
