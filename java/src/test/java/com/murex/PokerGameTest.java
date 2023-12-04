ec98b1d4ae2c783da126a93f6569a9c907684d0d
aatwi <ahmadatwi23@gmail.com>
2023-12-04 21:42:40 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 7
    test-stats:
        run: 33
        passed: 33
        failed: 0
        skipped: 1
        error: 0
        duration: 57ms

--- java/src/test/java/com/murex/PokerGameTest.java
+++ java/src/test/java/com/murex/PokerGameTest.java
@@ -104,4 +104,11 @@ public class PokerGameTest {
     PAIR_black_wins_with_ace_over_a_pair_of_ace_due_to_other_rank() {
         assertWinner("2H 2D 5S 6C KS", "2C 2S 6H 8H JC", "Black wins. - with Pair cards and higher rank: 2 and King");
     }
+    
+    @Disabled
+    @Test
+    public void
+    TWO_PAIR_black_wins_with_two_pairs() {
+        assertWinner("2H 2D 5S 5C KS", "2C 3S 6H 8H JC", "Black wins. - with two pairs: 2 and 5");
+    }
 }
