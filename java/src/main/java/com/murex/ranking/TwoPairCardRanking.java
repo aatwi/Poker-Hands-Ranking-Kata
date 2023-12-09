5513ae82b7e748cc4fadd3971b1e9fc108e6384e
aatwi <ahmadatwi23@gmail.com>
2023-12-09 16:56:53 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 6
        test: 0
    test-stats:
        run: 37
        passed: 37
        failed: 0
        skipped: 1
        error: 0
        duration: 54ms

--- java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
+++ java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
@@ -20,15 +20,15 @@ public class TwoPairCardRanking extends PokerHandRanking{
 
     @Override
     public Result getMatchingResult() {
+        List<Card> list = getTwoPairs(blackHand);
         if(blackHand.getCardAt(0).getValue().equals("7") && blackHand.getCardAt(1).getValue().equals("7")
         && blackHand.getCardAt(2).getValue().equals("Ten") && blackHand.getCardAt(3).getValue().equals("Ten")){
-            List<Card> list = getTwoPairs(blackHand);
             return Result.aMatchResult("Black wins. - with two pairs: "+list.get(0).getValue()+" and " + list.get(1).getValue());
         }
         if(whiteHand.getCardAt(0).getValue().equals("1") && whiteHand.getCardAt(1).getValue().equals("1")
         && whiteHand.getCardAt(3).getValue().equals("7") && whiteHand.getCardAt(4).getValue().equals("7")) {
-            List<Card> list = getTwoPairs(whiteHand);
-            return Result.aMatchResult("White wins. - with two pairs: "+list.get(0).getValue()+" and " + list.get(1).getValue());
+            List<Card> twoPairs = getTwoPairs(whiteHand);
+            return Result.aMatchResult("White wins. - with two pairs: "+twoPairs.get(0).getValue()+" and " + twoPairs.get(1).getValue());
         }
         return Result.aNoMatchResult();
     }
