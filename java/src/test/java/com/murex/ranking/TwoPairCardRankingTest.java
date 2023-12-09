c1ca89c06226408b9aa6c1e6033e3f550f3fc224
aatwi <ahmadatwi23@gmail.com>
2023-12-09 12:45:04 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 13
    test-stats:
        run: 37
        passed: 37
        failed: 0
        skipped: 1
        error: 0
        duration: 51ms

--- java/src/test/java/com/murex/ranking/TwoPairCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/TwoPairCardRankingTest.java
@@ -25,21 +25,20 @@ class TwoPairCardRankingTest {
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
         Assertions.assertEquals(Result.aMatchResult("White wins. - with two pairs: 1 and 7"), twoPairCardRanking.getMatchingResult());
     }
-
+    
     @Test
-    public void it_should_return_a_no_matching_result_when_none_have_two_pairs() {
-        Hand blackHand = Hand.buildFrom("Black", "1H 7C 8D TH AS");
-        Hand whiteHand = Hand.buildFrom("White", "2H 3C 6H 7D 8S");
+    public void it_should_return_a_no_matching_result_when_only_one_pair_exists() {
+        Hand blackHand = Hand.buildFrom("Black", "2H 7C 8D TH AS");
+        Hand whiteHand = Hand.buildFrom("White", "1H 1C 6H 7D 9S");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
         Assertions.assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
     }
 
-//    @Disabled
     @Test
-    public void it_should_return_a_no_matching_result_when_only_one_pair_exists() {
-        Hand blackHand = Hand.buildFrom("Black", "2H 7C 8D TH AS");
-        Hand whiteHand = Hand.buildFrom("White", "1H 1C 6H 7D 9S");
+    public void it_should_return_a_no_matching_result_when_none_have_two_pairs() {
+        Hand blackHand = Hand.buildFrom("Black", "1H 7C 8D TH AS");
+        Hand whiteHand = Hand.buildFrom("White", "2H 3C 6H 7D 8S");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
         Assertions.assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
