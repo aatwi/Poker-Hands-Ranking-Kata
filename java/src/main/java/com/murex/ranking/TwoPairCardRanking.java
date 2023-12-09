ee40cd9619669089445e0bd9a1c7206b65dc593e
aatwi <ahmadatwi23@gmail.com>
2023-12-09 12:48:47 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 5
        test: 0
    test-stats:
        run: 37
        passed: 37
        failed: 0
        skipped: 1
        error: 0
        duration: 57ms

--- java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
+++ java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
@@ -10,11 +10,12 @@ public class TwoPairCardRanking extends PokerHandRanking{
 
     @Override
     public Result getMatchingResult() {
-        if(blackHand.getCardAt(0).getValue().equals("7") && blackHand.getCardAt(1).getValue().equals("7")){
+        if(blackHand.getCardAt(0).getValue().equals("7") && blackHand.getCardAt(1).getValue().equals("7")
+        && blackHand.getCardAt(2).getValue().equals("Ten") && blackHand.getCardAt(3).getValue().equals("Ten")){
             return Result.aMatchResult("Black wins. - with two pairs: 7 and Ten");
         }
         if(whiteHand.getCardAt(0).getValue().equals("1") && whiteHand.getCardAt(1).getValue().equals("1")
-        & whiteHand.getCardAt(3).getValue().equals("7") && whiteHand.getCardAt(4).getValue().equals("7")) {
+        && whiteHand.getCardAt(3).getValue().equals("7") && whiteHand.getCardAt(4).getValue().equals("7")) {
             return Result.aMatchResult("White wins. - with two pairs: 1 and 7");
         }
         return Result.aNoMatchResult();
