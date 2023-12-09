a0886727878fd15a0003a7b31cf2900960b83e58
aatwi <ahmadatwi23@gmail.com>
2023-12-09 16:47:58 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 1
        test: 0
    test-stats:
        run: 37
        passed: 37
        failed: 0
        skipped: 1
        error: 0
        duration: 54ms

--- java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
+++ java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
@@ -23,7 +23,8 @@ public class TwoPairCardRanking extends PokerHandRanking{
         if(blackHand.getCardAt(0).getValue().equals("7") && blackHand.getCardAt(1).getValue().equals("7")
         && blackHand.getCardAt(2).getValue().equals("Ten") && blackHand.getCardAt(3).getValue().equals("Ten")){
             Map<Card, Long> collect = Arrays.stream(blackHand.getCards()).collect(groupingBy(Function.identity(), counting()));
-            List<Card> twoPairs = collect.keySet().stream().filter(x -> collect.get(x) == 2).toList(); 
+            List<Card> twoPairs = collect.keySet().stream().filter(x -> collect.get(x) == 2).toList();
+            List<Card> list = twoPairs.stream().sorted().toList();
             return Result.aMatchResult("Black wins. - with two pairs: 7 and Ten");
         }
         if(whiteHand.getCardAt(0).getValue().equals("1") && whiteHand.getCardAt(1).getValue().equals("1")
