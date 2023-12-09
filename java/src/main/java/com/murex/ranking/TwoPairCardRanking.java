3b2e630577812d03b568f46c465b18ebb79a2d53
aatwi <ahmadatwi23@gmail.com>
2023-12-09 12:44:46 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 3
        test: 0
    test-stats:
        run: 36
        passed: 36
        failed: 0
        skipped: 2
        error: 0
        duration: 46ms

--- java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
+++ java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
@@ -13,7 +13,8 @@ public class TwoPairCardRanking extends PokerHandRanking{
         if(blackHand.getCardAt(0).getValue().equals("7") && blackHand.getCardAt(1).getValue().equals("7")){
             return Result.aMatchResult("Black wins. - with two pairs: 7 and Ten");
         }
-        if(whiteHand.getCardAt(0).getValue().equals("1") && whiteHand.getCardAt(1).getValue().equals("1")) {
+        if(whiteHand.getCardAt(0).getValue().equals("1") && whiteHand.getCardAt(1).getValue().equals("1")
+        & whiteHand.getCardAt(3).getValue().equals("7") && whiteHand.getCardAt(4).getValue().equals("7")) {
             return Result.aMatchResult("White wins. - with two pairs: 1 and 7");
         }
         return Result.aNoMatchResult();
