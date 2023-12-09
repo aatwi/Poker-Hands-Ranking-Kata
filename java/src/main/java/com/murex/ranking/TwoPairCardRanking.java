73cf3dfc5f36a90b7e04dbb82b8df3da8f17342f
aatwi <ahmadatwi23@gmail.com>
2023-12-09 16:56:07 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 11
        test: 0
    test-stats:
        run: 37
        passed: 37
        failed: 0
        skipped: 1
        error: 0
        duration: 61ms

--- java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
+++ java/src/main/java/com/murex/ranking/TwoPairCardRanking.java
@@ -22,16 +22,19 @@ public class TwoPairCardRanking extends PokerHandRanking{
     public Result getMatchingResult() {
         if(blackHand.getCardAt(0).getValue().equals("7") && blackHand.getCardAt(1).getValue().equals("7")
         && blackHand.getCardAt(2).getValue().equals("Ten") && blackHand.getCardAt(3).getValue().equals("Ten")){
-            Map<Card, Long> collect = Arrays.stream(blackHand.getCards()).collect(groupingBy(Function.identity(), counting()));
-            List<Card> list = collect.keySet().stream().filter(x -> collect.get(x) == 2).sorted().toList();
+            List<Card> list = getTwoPairs(blackHand);
             return Result.aMatchResult("Black wins. - with two pairs: "+list.get(0).getValue()+" and " + list.get(1).getValue());
         }
         if(whiteHand.getCardAt(0).getValue().equals("1") && whiteHand.getCardAt(1).getValue().equals("1")
         && whiteHand.getCardAt(3).getValue().equals("7") && whiteHand.getCardAt(4).getValue().equals("7")) {
-            Map<Card, Long> collect = Arrays.stream(whiteHand.getCards()).collect(groupingBy(Function.identity(), counting()));
-            List<Card> list = collect.keySet().stream().filter(x -> collect.get(x) == 2).sorted().toList();
+            List<Card> list = getTwoPairs(whiteHand);
             return Result.aMatchResult("White wins. - with two pairs: "+list.get(0).getValue()+" and " + list.get(1).getValue());
         }
         return Result.aNoMatchResult();
+    }
+
+    private List<Card> getTwoPairs(Hand blackHand) {
+        Map<Card, Long> collect = Arrays.stream(blackHand.getCards()).collect(groupingBy(Function.identity(), counting()));
+        return collect.keySet().stream().filter(x -> collect.get(x) == 2).sorted().toList();
     }
 }
