94b09d21f48c6b0d6bb0060c27c75a00cab6cd35
aatwi <ahmadatwi23@gmail.com>
2023-12-10 15:41:10 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 33
    test-stats:
        run: 39
        passed: 39
        failed: 0
        skipped: 2
        error: 0
        duration: 58ms

--- java/src/test/java/com/murex/ranking/TwoPairCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/TwoPairCardRankingTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TwoPairCardRankingTest {
 
     @Test
@@ -14,7 +16,7 @@ class TwoPairCardRankingTest {
         Hand whiteHand = Hand.buildFrom("White", "1H 4C 5H 7D AD");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Assertions.assertEquals(Result.aMatchResult("Black wins. - with two pairs: 7 and Ten"), twoPairCardRanking.getMatchingResult());
+        assertEquals(Result.aMatchResult("Black wins. - with two pairs: 7 and Ten"), twoPairCardRanking.getMatchingResult());
     }
 
     @Test
@@ -23,7 +25,7 @@ class TwoPairCardRankingTest {
         Hand whiteHand = Hand.buildFrom("White", "1H 1C 6H 7D 7S");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Assertions.assertEquals(Result.aMatchResult("White wins. - with two pairs: 1 and 7"), twoPairCardRanking.getMatchingResult());
+        assertEquals(Result.aMatchResult("White wins. - with two pairs: 1 and 7"), twoPairCardRanking.getMatchingResult());
     }
 
     @Test
@@ -32,7 +34,7 @@ class TwoPairCardRankingTest {
         Hand whiteHand = Hand.buildFrom("White", "3H 3C 9H 9D TS");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Assertions.assertEquals(Result.aMatchResult("White wins. - with two pairs: 3 and 9"), twoPairCardRanking.getMatchingResult());
+        assertEquals(Result.aMatchResult("White wins. - with two pairs: 3 and 9"), twoPairCardRanking.getMatchingResult());
     }
 
     @Test
@@ -41,7 +43,15 @@ class TwoPairCardRankingTest {
         Hand whiteHand = Hand.buildFrom("White", "3H 3C 9H 9D TS");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Assertions.assertEquals(Result.aMatchResult("Black wins. - with two pairs: 2 and Ten"), twoPairCardRanking.getMatchingResult());
+        assertEquals(Result.aMatchResult("Black wins. - with two pairs: 2 and Ten"), twoPairCardRanking.getMatchingResult());
+    }
+
+    public void when_both_hands_has_two_pairs_the_higher_hand_wins_black_wins_higher_cards_are_equal() {
+        Hand blackHand = Hand.buildFrom("Black", "1H 1C 4D 4H AS");
+        Hand whiteHand = Hand.buildFrom("White", "2D 2S 4C 4S AH");
+
+        TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
+        assertEquals(Result.aMatchResult("White wins. - with two pairs: 2 and 4"), twoPairCardRanking.getMatchingResult());
     }
 
     @Test
@@ -50,7 +60,7 @@ class TwoPairCardRankingTest {
         Hand whiteHand = Hand.buildFrom("White", "1H 1C 6H 7D 9S");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Assertions.assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
+        assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
     }
 
     @Test
@@ -59,6 +69,17 @@ class TwoPairCardRankingTest {
         Hand whiteHand = Hand.buildFrom("White", "2H 3C 6H 7D 8S");
 
         TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Assertions.assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
+        assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
+    }
+
+
+    @Disabled
+    @Test
+    public void it_should_return_a_no_matching_result_when_two_hands_have_same_card_values() {
+        Hand blackHand = Hand.buildFrom("Black", "1H 1C 4D 4H AS");
+        Hand whiteHand = Hand.buildFrom("White", "1D 1S 4C 4S AH");
+
+        TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
+        assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
     }
 }
