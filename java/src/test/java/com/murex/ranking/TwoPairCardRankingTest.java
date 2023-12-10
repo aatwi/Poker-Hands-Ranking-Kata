568e9382be3d0c572f7146dac0f1375b79d289d3
aatwi <ahmadatwi23@gmail.com>
2023-12-10 21:40:12 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 27
    test-stats:
        run: 42
        passed: 42
        failed: 0
        skipped: 0
        error: 0
        duration: 90ms

--- java/src/test/java/com/murex/ranking/TwoPairCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/TwoPairCardRankingTest.java
@@ -16,6 +16,12 @@ class TwoPairCardRankingTest {
         assertEquals(aMatchResult(message), twoPairCardRanking.getMatchingResult());
     }
 
+    private static void assertNoMatchingResult(String blackHand, String whiteHand) {
+        TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(buildFrom("Black", blackHand), buildFrom("White", whiteHand));
+        
+        assertEquals(Result.aNoMatchResult(), twoPairCardRanking.getMatchingResult());
+    }
+
     @Test
     public void it_should_return_a_matching_result_when_the_black_player_has_two_pairs() {
         assertMatchingResult("7H 7C TD TH AS", "1H 4C 5H 7D AD", "Black wins. - with two pairs: 7 and Ten");
@@ -48,32 +54,17 @@ class TwoPairCardRankingTest {
 
     @Test
     public void it_should_return_a_no_matching_result_when_only_one_pair_exists() {
-        Hand blackHand = buildFrom("Black", "2H 7C 8D TH AS");
-        Hand whiteHand = buildFrom("White", "1H 1C 6H 7D 9S");
-
-        TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Result expectedMessage = Result.aNoMatchResult();
-        assertEquals(expectedMessage, twoPairCardRanking.getMatchingResult());
+        assertNoMatchingResult("2H 7C 8D TH AS", "1H 1C 6H 7D 9S");
     }
 
     @Test
     public void it_should_return_a_no_matching_result_when_none_have_two_pairs() {
-        Hand blackHand = buildFrom("Black", "1H 7C 8D TH AS");
-        Hand whiteHand = buildFrom("White", "2H 3C 6H 7D 8S");
-
-        TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Result expectedMessage = Result.aNoMatchResult();
-        assertEquals(expectedMessage, twoPairCardRanking.getMatchingResult());
+        assertNoMatchingResult("1H 7C 8D TH AS", "2H 3C 6H 7D 8S");
     }
 
 
     @Test
     public void it_should_return_a_no_matching_result_when_two_hands_have_same_card_values() {
-        Hand blackHand = buildFrom("Black", "1H 1C 4D 4H AS");
-        Hand whiteHand = buildFrom("White", "1D 1S 4C 4S AH");
-
-        TwoPairCardRanking twoPairCardRanking = new TwoPairCardRanking(blackHand, whiteHand);
-        Result expectedMessage = Result.aNoMatchResult();
-        assertEquals(expectedMessage, twoPairCardRanking.getMatchingResult());
+        assertNoMatchingResult("1H 1C 4D 4H AS", "1D 1S 4C 4S AH");
     }
 }
