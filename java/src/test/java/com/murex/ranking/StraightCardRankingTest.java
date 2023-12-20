4d7eeedb5975db9755996a6e448a50897f8e4898
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-12-20 17:41:32 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 4
    test-stats:
        run: 52
        passed: 52
        failed: 0
        skipped: 3
        error: 0
        duration: 94ms

--- java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
@@ -1,6 +1,8 @@
 package com.murex.ranking;
 
 import com.murex.Hand;
+import com.murex.Result;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +13,9 @@ class StraightCardRankingTest {
     public void it_should_return_a_matching_result_with_black_as_winner_having_straight_cards() {
         Hand blackHand = Hand.buildFrom("Black", "7H 8C TD KH AS");
         Hand whiteHand = Hand.buildFrom("White", "2D 3H 5C 9S KH");
-        
+
+        StraightCardRanking straightCardRanking = new StraightCardRanking(blackHand, whiteHand);
+        Assertions.assertEquals(Result.aNoMatchResult(), straightCardRanking.getMatchingResult());
     }
 
 }
