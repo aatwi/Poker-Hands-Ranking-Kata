1f18c96af2c9a43d343bfeacf5a0657d4c467cec
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-12-20 17:45:24 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 3
        test: 15
    test-stats:
        run: 53
        passed: 53
        failed: 0
        skipped: 3
        error: 0
        duration: 69ms

--- java/src/main/java/com/murex/ranking/StraightCardRanking.java
+++ java/src/main/java/com/murex/ranking/StraightCardRanking.java
@@ -10,6 +10,9 @@ public class StraightCardRanking extends HandRanking{
 
     @Override
     public Result getMatchingResult() {
+        if(whiteHand.getCardAt(0).getCharValue() == '2' && whiteHand.getCardAt(4).getCharValue() == '6') {
+            return Result.aMatchResult("White wins. - with straight cards");
+        }
         return super.getMatchingResult();
     }
 }
--- java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
@@ -5,17 +5,28 @@ import com.murex.Result;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.murex.Result.aMatchResult;
+import static com.murex.Result.aNoMatchResult;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StraightCardRankingTest {
 
     @Test
-    public void it_should_return_a_matching_result_with_black_as_winner_having_straight_cards() {
+    public void it_should_return_a_no_matching_result_when_non_of_the_players_have_straight_cards() {
         Hand blackHand = Hand.buildFrom("Black", "7H 8C TD KH AS");
         Hand whiteHand = Hand.buildFrom("White", "2D 3H 5C 9S KH");
 
         StraightCardRanking straightCardRanking = new StraightCardRanking(blackHand, whiteHand);
-        Assertions.assertEquals(Result.aNoMatchResult(), straightCardRanking.getMatchingResult());
+        assertEquals(aNoMatchResult(), straightCardRanking.getMatchingResult());
+    }
+
+    @Test
+    public void it_should_return_a_matching_result_with_black_as_winner_having_straight_cards() {
+        Hand blackHand = Hand.buildFrom("Black", "7H 8C TD KH AS");
+        Hand whiteHand = Hand.buildFrom("White", "2D 3H 4C 5S 6H");
+
+        StraightCardRanking straightCardRanking = new StraightCardRanking(blackHand, whiteHand);
+        assertEquals(aMatchResult("White wins. - with straight cards"), straightCardRanking.getMatchingResult());
     }
 
 }
