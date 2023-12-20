abedbd596c8c9909dd4e10c4d358fa4d8c61a963
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-12-20 16:52:49 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 36
    test-stats:
        run: 51
        passed: 51
        failed: 0
        skipped: 0
        error: 0
        duration: 75ms

--- java/src/test/java/com/murex/ranking/ThreeOfAKindRankingTest.java
+++ java/src/test/java/com/murex/ranking/ThreeOfAKindRankingTest.java
@@ -5,15 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import static com.murex.Result.aMatchResult;
 import static com.murex.Result.aNoMatchResult;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ThreeOfAKindRankingTest {
-
-    @Test
-    public void it_should_return_a_non_matching_result_when_both_hands_have_no_three_cards_with_same_kind() {
-        ThreeOfAKindRanking threeOfAKindRanking = buildThreeOfAKindRanking("7H 8C TD KH AS", "2D 3H 5C 9S KH");
-        assertEquals(aNoMatchResult(), threeOfAKindRanking.getMatchingResult());
-    }
 
     private static ThreeOfAKindRanking buildThreeOfAKindRanking(String blackHandCards, String whiteHandCards) {
         Hand blackHand = Hand.buildFrom("Black", blackHandCards);
@@ -21,34 +15,42 @@ class ThreeOfAKindRankingTest {
         return new ThreeOfAKindRanking(blackHand, whiteHand);
     }
 
+    private static void assertNoMatchingResult(String blackHandCards, String whiteHandCards) {
+        assertEquals(aNoMatchResult(), buildThreeOfAKindRanking(blackHandCards, whiteHandCards).getMatchingResult());
+    }
+
+    private static void assertMatchingResult(String blackHandCards, String whiteHandCards, String message) {
+        assertEquals(aMatchResult(message), buildThreeOfAKindRanking(blackHandCards, whiteHandCards).getMatchingResult());
+    }
+
+    @Test
+    public void it_should_return_a_non_matching_result_when_both_hands_have_no_three_cards_with_same_kind() {
+        assertNoMatchingResult("7H 8C TD KH AS", "2D 3H 5C 9S KH");
+    }
+
     @Test
     public void it_should_return_a_matching_result_with_white_as_winner_having_three_cards_of_9() {
-        ThreeOfAKindRanking threeOfAKindRanking = buildThreeOfAKindRanking("7H 7C TD KH AS", "2D 9H 9C 9S KH");
-        assertEquals(aMatchResult("White wins. - with three of a kind: 9"), threeOfAKindRanking.getMatchingResult());
+        assertMatchingResult("7H 7C TD KH AS", "2D 9H 9C 9S KH", "White wins. - with three of a kind: 9");
     }
 
     @Test
     public void it_should_return_a_matching_result_with_white_as_winner_having_three_cards_of_10() {
-        ThreeOfAKindRanking threeOfAKindRanking = buildThreeOfAKindRanking("7H 7C TD KH AS", "2D 9H TC TS TH");
-        assertEquals(aMatchResult("White wins. - with three of a kind: Ten"), threeOfAKindRanking.getMatchingResult());
+        assertMatchingResult("7H 7C TD KH AS", "2D 9H TC TS TH", "White wins. - with three of a kind: Ten");
     }
 
     @Test
     public void it_should_return_a_matching_result_with_black_as_winner_having_three_cards_of_7() {
-        ThreeOfAKindRanking threeOfAKindRanking = buildThreeOfAKindRanking("7H 7C 7D KH AS", "2D 9H 8C TS KH");
-        assertEquals(aMatchResult("Black wins. - with three of a kind: 7"), threeOfAKindRanking.getMatchingResult());
+        assertMatchingResult("7H 7C 7D KH AS", "2D 9H 8C TS KH", "Black wins. - with three of a kind: 7");
     }
 
     @Test
     public void it_should_return_a_matching_result_with_black_as_winner_having_three_cards_of_Ace_over_white_having_three_cards_of_9() {
-        ThreeOfAKindRanking threeOfAKindRanking = buildThreeOfAKindRanking("2H 3C AD AH AS", "2D 9H 9C 9S KH");
-        assertEquals(aMatchResult("Black wins. - with three of a kind: Ace"), threeOfAKindRanking.getMatchingResult());
+        assertMatchingResult("2H 3C AD AH AS", "2D 9H 9C 9S KH", "Black wins. - with three of a kind: Ace");
     }
 
     @Test
     public void it_should_return_a_matching_result_with_white_as_winner_having_three_cards_of_Jack_over_black_having_three_cards_of_8() {
-        ThreeOfAKindRanking threeOfAKindRanking = buildThreeOfAKindRanking("2H 3C 8D 8H 8S", "2D JH JC JS KH");
-        assertEquals(aMatchResult("White wins. - with three of a kind: Jack"), threeOfAKindRanking.getMatchingResult());
+        assertMatchingResult("2H 3C 8D 8H 8S", "2D JH JC JS KH", "White wins. - with three of a kind: Jack");
     }
 
 }
