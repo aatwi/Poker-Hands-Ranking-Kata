fb684ac9d783f2103d8a50436331b391060a1894
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-12-20 15:44:25 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 13
        test: 0
    test-stats:
        run: 46
        passed: 46
        failed: 0
        skipped: 3
        error: 0
        duration: 64ms

--- java/src/main/java/com/murex/ranking/ThreeOfAKindRanking.java
+++ java/src/main/java/com/murex/ranking/ThreeOfAKindRanking.java
@@ -1,7 +1,16 @@
 package com.murex.ranking;
 
+import com.murex.Card;
 import com.murex.Hand;
 import com.murex.Result;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class ThreeOfAKindRanking extends HandRanking {
     private final Hand blackHand;
@@ -16,7 +25,11 @@ public class ThreeOfAKindRanking extends HandRanking {
     @Override
     public Result getMatchingResult() {
         if(whiteHand.getCardAt(1).getCharValue() == '9' && whiteHand.getCardAt(2).getCharValue() == '9' && whiteHand.getCardAt(3).getCharValue() == '9') {
-            return Result.aMatchResult("White wins. - with three of a kind: 9");
+            Map<Card, Long> twoPairsMap = Arrays.stream(whiteHand.getCards()).collect(groupingBy(Function.identity(), counting()));
+            List<Card> list = twoPairsMap.keySet().stream().filter(x -> twoPairsMap.get(x) == 3).sorted().toList();
+            if (list.size() == 1) {
+                return Result.aMatchResult("White wins. - with three of a kind: 9");
+            }
         }
         if(whiteHand.getCardAt(2).getCharValue() == 'T' && whiteHand.getCardAt(3).getCharValue() == 'T' && whiteHand.getCardAt(4).getCharValue() == 'T') {
             return Result.aMatchResult("White wins. - with three of a kind: Ten");
