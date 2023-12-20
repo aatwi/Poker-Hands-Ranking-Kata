3d55685e5eb58c73f55b7f9da641c1d75bed4ae6
Ahmad ATWI <ahmadatwi23@gmail.com>
2023-12-20 17:51:18 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 3
        test: 11
    test-stats:
        run: 54
        passed: 54
        failed: 0
        skipped: 3
        error: 0
        duration: 66ms

--- java/src/main/java/com/murex/ranking/StraightCardRanking.java
+++ java/src/main/java/com/murex/ranking/StraightCardRanking.java
@@ -13,6 +13,9 @@ public class StraightCardRanking extends HandRanking{
         if(whiteHand.getCardAt(0).getCharValue() == '2' && whiteHand.getCardAt(4).getCharValue() == '6') {
             return Result.aMatchResult("White wins. - with straight cards");
         }
+        if(blackHand.getCardAt(0).getCharValue() == '7' && blackHand.getCardAt(4).getCharValue() == 'J') {
+            return Result.aMatchResult("Black wins. - with straight cards");
+        }
         return super.getMatchingResult();
     }
 }
--- java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
@@ -21,12 +21,21 @@ class StraightCardRankingTest {
     }
 
     @Test
-    public void it_should_return_a_matching_result_with_black_as_winner_having_straight_cards() {
+    public void it_should_return_a_matching_result_with_white_as_winner_having_straight_cards_2_to_6() {
         Hand blackHand = Hand.buildFrom("Black", "7H 8C TD KH AS");
         Hand whiteHand = Hand.buildFrom("White", "2D 3H 4C 5S 6H");
 
         StraightCardRanking straightCardRanking = new StraightCardRanking(blackHand, whiteHand);
         assertEquals(aMatchResult("White wins. - with straight cards"), straightCardRanking.getMatchingResult());
+    }
+
+    @Test
+    public void it_should_return_a_matching_result_with_black_as_winner_having_straight_cards_7_to_Jack() {
+        Hand blackHand = Hand.buildFrom("Black", "7H 8C 9D TH JS");
+        Hand whiteHand = Hand.buildFrom("White", "2D 3H 7C JH AD");
+
+        StraightCardRanking straightCardRanking = new StraightCardRanking(blackHand, whiteHand);
+        assertEquals(aMatchResult("Black wins. - with straight cards"), straightCardRanking.getMatchingResult());
     }
 
 }
