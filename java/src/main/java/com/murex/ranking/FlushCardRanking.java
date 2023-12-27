7349cb7bc53a3c142689fefb2544e4361d31887e
aatwi <ahmadatwi23@gmail.com>
2023-12-27 17:35:05 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 42
        test: 0
    test-stats:
        run: 67
        passed: 67
        failed: 0
        skipped: 3
        error: 0
        duration: 73ms

--- java/src/main/java/com/murex/ranking/FlushCardRanking.java
+++ java/src/main/java/com/murex/ranking/FlushCardRanking.java
@@ -10,48 +10,30 @@ public class FlushCardRanking extends HandRanking {
 
     @Override
     public Result getMatchingResult() {
-        boolean allSameSuite = false;
-        for (int index = 1; index < 5; index++) {
-            if (blackHand.getCardAt(index-1).getSuite() != blackHand.getCardAt(index).getSuite()) {
-                allSameSuite = false;
-                break;
-            }
-            allSameSuite = true;
-        }
+        boolean allSameSuite = isFlush(blackHand);
 
         if(allSameSuite) {
             return Result.aMatchResult("Black wins. - with flush");
         }
 
-        boolean allWhiteSameSuite = false;
-        for (int index = 1; index < 5; index++) {
-            if (whiteHand.getCardAt(index-1).getSuite() != whiteHand.getCardAt(index).getSuite()) {
-                allWhiteSameSuite = false;
-                break;
-            }
-            allWhiteSameSuite = true;
-        }
+        boolean allWhiteSameSuite = isFlush(whiteHand);
 
         if(allWhiteSameSuite) {
             return Result.aMatchResult("White wins. - with flush");
         }
 
-        if (blackHand.getCardAt(0).getSuite() == 'H' && blackHand.getCardAt(1).getSuite() == 'H' && blackHand.getCardAt(2).getSuite() == 'H'
-        && blackHand.getCardAt(3).getSuite() == 'H' && blackHand.getCardAt(4).getSuite() == 'H'){
-            return Result.aMatchResult("Black wins. - with flush");
-        }
-        if (blackHand.getCardAt(0).getSuite() == 'D' && blackHand.getCardAt(1).getSuite() == 'D' && blackHand.getCardAt(2).getSuite() == 'D'
-                && blackHand.getCardAt(3).getSuite() == 'D' && blackHand.getCardAt(4).getSuite() == 'D'){
-            return Result.aMatchResult("Black wins. - with flush");
-        }
-        if (blackHand.getCardAt(0).getSuite() == 'C' && blackHand.getCardAt(1).getSuite() == 'C' && blackHand.getCardAt(2).getSuite() == 'C'
-                && blackHand.getCardAt(3).getSuite() == 'C' && blackHand.getCardAt(4).getSuite() == 'C'){
-            return Result.aMatchResult("Black wins. - with flush");
-        }
-        if (blackHand.getCardAt(0).getSuite() == 'S' && blackHand.getCardAt(1).getSuite() == 'S' && blackHand.getCardAt(2).getSuite() == 'S'
-                && blackHand.getCardAt(3).getSuite() == 'S' && blackHand.getCardAt(4).getSuite() == 'S'){
-            return Result.aMatchResult("Black wins. - with flush");
-        }
         return super.getMatchingResult();
+    }
+
+    private boolean isFlush(Hand blackHand) {
+        boolean allSameSuite = false;
+        for (int index = 1; index < 5; index++) {
+            if (blackHand.getCardAt(index - 1).getSuite() != blackHand.getCardAt(index).getSuite()) {
+                allSameSuite = false;
+                break;
+            }
+            allSameSuite = true;
+        }
+        return allSameSuite;
     }
 }
