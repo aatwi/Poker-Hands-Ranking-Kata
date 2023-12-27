ece91279206645fa72492577c518cda5fa544f90
aatwi <ahmadatwi23@gmail.com>
2023-12-27 12:55:09 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 36
        test: 0
    test-stats:
        run: 56
        passed: 56
        failed: 0
        skipped: 3
        error: 0
        duration: 104ms

--- java/src/main/java/com/murex/ranking/StraightCardRanking.java
+++ java/src/main/java/com/murex/ranking/StraightCardRanking.java
@@ -10,26 +10,8 @@ public class StraightCardRanking extends HandRanking{
 
     @Override
     public Result getMatchingResult() {
-        boolean straightWhite = false;
-        boolean straightBlack = false;
-        for (int i = 1; i < whiteHand.getCards().length; i++) {
-            int previousCard = whiteHand.getCardAt(i - 1).getIntValue();
-            int currentCard = whiteHand.getCardAt(i).getIntValue();
-            if(currentCard != previousCard + 1){
-                straightWhite = false;
-                break;
-            }
-            straightWhite = true;
-        }
-        for (int i = 1; i < blackHand.getCards().length; i++) {
-            int previousCard = blackHand.getCardAt(i - 1).getIntValue();
-            int currentCard = blackHand.getCardAt(i).getIntValue();
-            if(currentCard != previousCard + 1){
-                straightBlack = false;
-                break;
-            }
-            straightBlack = true;
-        }
+        boolean straightBlack = isAStraight(blackHand);
+        boolean straightWhite = isAStraight(whiteHand);
         if(straightWhite) {
             return Result.aMatchResult("White wins. - with straight cards");
         }
@@ -37,5 +19,19 @@ public class StraightCardRanking extends HandRanking{
             return Result.aMatchResult("Black wins. - with straight cards");
         }
         return super.getMatchingResult();
+    }
+
+    private boolean isAStraight(Hand hand) {
+        boolean straightWhite = false;
+        for (int i = 1; i < hand.getCards().length; i++) {
+            int previousCard = hand.getCardAt(i - 1).getIntValue();
+            int currentCard = hand.getCardAt(i).getIntValue();
+            if(currentCard != previousCard + 1){
+                straightWhite = false;
+                break;
+            }
+            straightWhite = true;
+        }
+        return straightWhite;
     }
 }
