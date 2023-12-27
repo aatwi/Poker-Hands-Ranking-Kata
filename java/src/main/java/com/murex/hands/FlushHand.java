afc307c05ad6537630340db8780ecdf663275f83
aatwi <ahmadatwi23@gmail.com>
2023-12-27 17:37:39 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 34
        test: 0
    test-stats:
        run: 67
        passed: 67
        failed: 0
        skipped: 3
        error: 0
        duration: 62ms

---
+++ java/src/main/java/com/murex/hands/FlushHand.java
@@ -0,0 +1,15 @@
+package com.murex.hands;
+
+import com.murex.Hand;
+
+public final class FlushHand {
+    private final Hand hand;
+
+    public FlushHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+}
--- java/src/main/java/com/murex/ranking/FlushCardRanking.java
+++ java/src/main/java/com/murex/ranking/FlushCardRanking.java
@@ -2,32 +2,35 @@ package com.murex.ranking;
 
 import com.murex.Hand;
 import com.murex.Result;
+import com.murex.hands.FlushHand;
 
 public class FlushCardRanking extends HandRanking {
+
+    private final FlushHand blackFlushHand;
+    private final FlushHand whiteFlushHand;
+
     public FlushCardRanking(Hand blackHand, Hand whiteHand) {
         super(blackHand, whiteHand);
+        blackFlushHand = new FlushHand(blackHand);
+        whiteFlushHand = new FlushHand(whiteHand);
     }
 
     @Override
     public Result getMatchingResult() {
-        boolean allSameSuite = isFlush(blackHand);
-
-        if(allSameSuite) {
+        if(isFlush(blackHand, blackFlushHand)) {
             return Result.aMatchResult("Black wins. - with flush");
         }
 
-        boolean allWhiteSameSuite = isFlush(whiteHand);
-
-        if(allWhiteSameSuite) {
+        if(isFlush(whiteHand, whiteFlushHand)) {
             return Result.aMatchResult("White wins. - with flush");
         }
 
         return super.getMatchingResult();
     }
 
-    private boolean isFlush(Hand blackHand) {
+    public boolean isFlush(Hand blackHand, FlushHand flushHand) {
         for (int index = 1; index < 5; index++) {
-            if (blackHand.getCardAt(index - 1).getSuite() != blackHand.getCardAt(index).getSuite()) {
+            if (flushHand.getHand().getCardAt(index - 1).getSuite() != flushHand.getHand().getCardAt(index).getSuite()) {
                 return false;
             }
         }
