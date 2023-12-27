f04063d07f12ff1d2a1e64d92907c8527077cb0a
aatwi <ahmadatwi23@gmail.com>
2023-12-27 15:58:17 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 30
    test-stats:
        run: 61
        passed: 61
        failed: 0
        skipped: 0
        error: 0
        duration: 75ms

--- java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
+++ java/src/test/java/com/murex/ranking/StraightCardRankingTest.java
@@ -10,17 +10,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StraightCardRankingTest {
 
-    @Test
-    public void it_should_return_a_no_matching_result_when_non_of_the_players_have_straight_cards() {
-        assertNonMatchingResult("7H 8C TD KH AS", "2D 3H 5C 9S KH");//, aNoMatchResult(), "White wins. - with straight cards and higher cards");
-    }
     private static void assertNonMatchingResult(String blackCards, String whiteCards) {
+        StraightCardRanking straightCardRanking = buildStraightCardRanking(blackCards, whiteCards);
+        assertEquals(aNoMatchResult(), straightCardRanking.getMatchingResult());
+    }
+
+    private static void assertMatchingResult(String blackCards, String whiteCards, Result expected, String s) {
+        StraightCardRanking straightCardRanking = buildStraightCardRanking(blackCards, whiteCards);
+        assertEquals(expected, straightCardRanking.getMatchingResult());
+    }
+    
+    private static StraightCardRanking buildStraightCardRanking(String blackCards, String whiteCards) {
         Hand blackHand = Hand.buildFrom("Black", blackCards);
         Hand whiteHand = Hand.buildFrom("White", whiteCards);
 
         StraightCardRanking straightCardRanking = new StraightCardRanking(blackHand, whiteHand);
-        assertEquals(aNoMatchResult(), straightCardRanking.getMatchingResult());
+        return straightCardRanking;
     }
+
+    @Test
+    public void it_should_return_a_no_matching_result_when_non_of_the_players_have_straight_cards() {
+        assertNonMatchingResult("7H 8C TD KH AS", "2D 3H 5C 9S KH");
+    }
+
     @Test
     public void it_should_return_a_matching_result_with_white_as_winner_having_straight_cards_2_to_6() {
         assertMatchingResult("7H 8C TD KH AS", "2D 3H 4C 5S 6H", aMatchResult("White wins. - with straight cards"), "White wins. - with straight cards");
@@ -38,15 +50,7 @@ class StraightCardRankingTest {
 
     @Test
     public void it_should_return_a_matching_result_with_black_as_winner_having_straight_cards_3_to_7() {
-         assertMatchingResult("3H 4C 5D 6H 7S", "2D 3H 7C JH AD", aMatchResult("Black wins. - with straight cards"), "Black wins. - with straight cards");
-    }
-
-    private static void assertMatchingResult(String blackCards, String whiteCards, Result expected, String s) {
-        Hand blackHand = Hand.buildFrom("Black", blackCards);
-        Hand whiteHand = Hand.buildFrom("White", whiteCards);
-
-        StraightCardRanking straightCardRanking = new StraightCardRanking(blackHand, whiteHand);
-        assertEquals(expected, straightCardRanking.getMatchingResult());
+        assertMatchingResult("3H 4C 5D 6H 7S", "2D 3H 7C JH AD", aMatchResult("Black wins. - with straight cards"), "Black wins. - with straight cards");
     }
 
     @Test
