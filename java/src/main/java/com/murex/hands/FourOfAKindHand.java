f74cb9ea2ddfaaeca913b051bf2e2c8024044a41
aatwi <ahmadatwi23@gmail.com>
2023-12-29 15:23:38 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 10
        test: 0
    test-stats:
        run: 83
        passed: 83
        failed: 0
        skipped: 5
        error: 0
        duration: 85ms

--- java/src/main/java/com/murex/hands/FourOfAKindHand.java
+++ java/src/main/java/com/murex/hands/FourOfAKindHand.java
@@ -5,6 +5,7 @@ import com.murex.Hand;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 import static java.util.stream.Collectors.counting;
@@ -12,9 +13,11 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class FourOfAKindHand {
     private final Hand hand;
+    private Optional<Card> fourCards;
 
     public FourOfAKindHand(Hand hand) {
         this.hand = hand;
+        this.fourCards = extractFourOfAKindCard();
     }
 
     public Hand getHand() {
@@ -22,7 +25,12 @@ public class FourOfAKindHand {
     }
 
     public boolean hasFourOfAKind() {
+        return fourCards.isPresent();
+    }
+
+    private Optional<Card> extractFourOfAKindCard() {
         Map<Card, Long> cardGroupsMap = Arrays.stream(getHand().getCards()).collect(groupingBy(Function.identity(), counting()));
-        return cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 4).findAny().isPresent();
+        Optional<Card> fourCards = cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 4).findAny();
+        return fourCards;
     }
 }
