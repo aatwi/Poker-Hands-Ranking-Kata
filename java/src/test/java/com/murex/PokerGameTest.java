3efa66d6c55e982c56d1a6b4001b13e9b5ba6468
aatwi <ahmadatwi23@gmail.com>
2023-12-29 16:14:30 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 12
    test-stats:
        run: 98
        passed: 98
        failed: 0
        skipped: 3
        error: 0
        duration: 58ms

--- java/src/test/java/com/murex/PokerGameTest.java
+++ java/src/test/java/com/murex/PokerGameTest.java
@@ -178,9 +178,21 @@ public class PokerGameTest {
     public void STRAIGHT_FLUSH_black_wins_and_higher_hand() {
         assertWinner("8D 9D TD JD QD", "2H 3H 4H 5H 6H", "Black wins. - with straight flush and higher hand");
     }
-    
+
     @Test
     public void STRAIGHT_FLUSH_white_wins_and_higher_hand() {
         assertWinner("2H 3H 4H 5H 6H","8D 9D TD JD QD",  "White wins. - with straight flush and higher hand");
+    }
+
+    @Disabled
+    @Test
+    public void ROYAL_FLUSH_white_wins() {
+        assertWinner("2H 3H 4H 5H 6H","TD JD QD KD AD",  "White wins. - with royal flush");
+    }
+    
+    @Disabled
+    @Test
+    public void ROYAL_FLUSH_black_wins() {
+        assertWinner("TH JH QH KH AH", "2H 3H 4H 5H 6H",  "White wins. - with royal flush");
     }
 }
