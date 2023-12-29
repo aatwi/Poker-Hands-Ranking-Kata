3538d83ede2149b0e2cc5d72e656aa2cc7fc53f8
aatwi <ahmadatwi23@gmail.com>
2023-12-29 15:39:13 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 90
    test-stats:
        run: 88
        passed: 88
        failed: 0
        skipped: 3
        error: 0
        duration: 98ms

--- java/src/test/java/com/murex/PokerGameTest.java
+++ java/src/test/java/com/murex/PokerGameTest.java
@@ -34,158 +34,144 @@ public class PokerGameTest {
     }
 
     @Test
-    public void
-    HIGH_CARD_white_wins_with_Ace() {
+    public void HIGH_CARD_white_wins_with_Ace() {
         assertWinner("2H 3D 5S 9C KD", "2C 3H 4S 8C AH", "White wins. - with high card: Ace");
     }
 
     @Test
-    public void
-    HIGH_CARD_black_wins_with_Queen() {
+    public void HIGH_CARD_black_wins_with_Queen() {
         assertWinner("2H 3D 5S 9C QD", "2C 3H 4S 8C JH", "Black wins. - with high card: Queen");
     }
 
     @Test
-    public void
-    HIGH_CARD_white_wins_Jack() {
+    public void HIGH_CARD_white_wins_Jack() {
         assertWinner("2H 3D 5S 7C 9D", "2C 3H 4S 8C JH", "White wins. - with high card: Jack");
     }
 
     @Test
-    public void
-    HIGH_CARD_black_wins_with_9() {
+    public void HIGH_CARD_black_wins_with_9() {
         assertWinner("2H 3D 5S 8C 9D", "2C 3H 4S 7C 8H", "Black wins. - with high card: 9");
     }
 
     @Test
-    public void
-    HIGH_CARD_black_wins_with_7() {
+    public void HIGH_CARD_black_wins_with_7() {
         assertWinner("2H 3D 5S 8C 9D", "2C 3H 4S 7C 9H", "Black wins. - with high card: 8");
     }
 
     @Test
-    public void
-    TIE_between_two_hands() {
+    public void TIE_between_two_hands() {
         assertWinner("2H 3D 5S 9C KD", "2D 3H 5C 9S KH", "Tie.");
     }
 
     @Test
-    public void
-    PAIR_black_wins_with_ace_as_pair() {
+    public void PAIR_black_wins_with_ace_as_pair() {
         assertWinner("7H JC KD AH AS", "2D 3H 5C 9S KH", "Black wins. - with Pair cards: Ace");
     }
 
     @Test
-    public void
-    PAIR_black_wins_with_jack_as_pair() {
+    public void PAIR_black_wins_with_jack_as_pair() {
         assertWinner("7H JH JC KD AS", "2D 3H 5C 9S KH", "Black wins. - with Pair cards: Jack");
     }
 
     @Test
-    public void
-    PAIR_white_wins_with_4_as_pair() {
+    public void PAIR_white_wins_with_4_as_pair() {
         assertWinner("5D 6C 7H JH AS", "2D 4H 4C 9S KH", "White wins. - with Pair cards: 4");
     }
 
     @Test
-    public void
-    PAIR_white_wins_with_ace_over_a_pair_of_2() {
+    public void PAIR_white_wins_with_ace_over_a_pair_of_2() {
         assertWinner("2H 2D 5S 6C AS", "4D 9S KH AH AC", "White wins. - with Pair cards: Ace");
     }
 
     @Test
-    public void
-    PAIR_white_wins_with_ace_over_a_pair_of_ace_due_to_other_rank() {
+    public void PAIR_white_wins_with_ace_over_a_pair_of_ace_due_to_other_rank() {
         assertWinner("2H 2D 5S 6C JS", "2C 2S 6H 8H AC", "White wins. - with high card: Ace");
     }
 
     @Test
-    public void
-    PAIR_black_wins_with_king_over_a_pair_of_ace_due_to_higher_rank() {
+    public void PAIR_black_wins_with_king_over_a_pair_of_ace_due_to_higher_rank() {
         assertWinner("2H 2D 5S 6C KS", "2C 2S 6H 8H JC", "Black wins. - with high card: King");
     }
 
     @Test
-    public void
-    TWO_PAIR_black_wins_with_two_pairs() {
+    public void TWO_PAIR_black_wins_with_two_pairs() {
         assertWinner("2H 2D 5S 5C KS", "2C 3S 6H 8H JC", "Black wins. - with two pairs: 2 and 5");
     }
 
     @Test
-    public void
-    THREE_OF_A_KIND_black_wins_having_three_cards_of_one_kind() {
+    public void THREE_OF_A_KIND_black_wins_having_three_cards_of_one_kind() {
         assertWinner("2H 2D 2S 5C KS", "2C 3S 6H 8H JC", "Black wins. - with three of a kind: 2");
     }
 
     @Test
-    public void
-    THREE_OF_A_KIND_white_wins_having_three_cards_of_one_kind() {
+    public void THREE_OF_A_KIND_white_wins_having_three_cards_of_one_kind() {
         assertWinner("2H 2S 4D 5C KS", "2C 3S 6H 6D 6C", "White wins. - with three of a kind: 6");
     }
 
     @Test
-    public void
-    THREE_OF_A_KIND_black_wins_having_three_cards_of_one_kind_and_higher_card() {
+    public void THREE_OF_A_KIND_black_wins_having_three_cards_of_one_kind_and_higher_card() {
         assertWinner("2H 5D 5S 5C KS", "2C 3S JH JD JC", "White wins. - with three of a kind: Jack");
     }
 
     @Test
-    public void
-    STRAIGHT_black_wins_with_a_straight_hand() {
+    public void STRAIGHT_black_wins_with_a_straight_hand() {
         assertWinner("2H 3D 4S 5C 6S", "2C 3S 6H 8H JC", "Black wins. - with straight cards");
     }
 
     @Test
-    public void
-    STRAIGHT_white_wins_with_a_straight_hand() {
+    public void STRAIGHT_white_wins_with_a_straight_hand() {
         assertWinner("2H 2S 4D 5C KS", "7C 8S 9H TD JC", "White wins. - with straight cards");
     }
 
     @Test
-    public void
-    STRAIGHT_white_wins_with_a_straight_and_higher_cards() {
+    public void STRAIGHT_white_wins_with_a_straight_and_higher_cards() {
         assertWinner("2H 3D 4S 5C 6S", "6C 7S 8H 9D TC", "White wins. - with straight cards and higher cards");
     }
 
     @Test
-    public void
-    FULL_HOUSE_white_wins_with_a_full_house_cards() {
-        assertWinner( "6C 7S 8H 9D TC","3H 3S AC AD AH", "White wins. - with full house");
+    public void FULL_HOUSE_white_wins_with_a_full_house_cards() {
+        assertWinner("6C 7S 8H 9D TC", "3H 3S AC AD AH", "White wins. - with full house");
     }
 
     @Test
-    public void
-    FLUSH_black_wins_with_a_full_house_cards() {
+    public void FLUSH_black_wins_with_a_full_house_cards() {
         assertWinner("2H 2S 2D JH JS", "6C 7S 8H 9D TC", "Black wins. - with full house");
     }
 
     @Test
-    public void
-    FLUSH_white_wins_with_a_full_house_and_higher_cards() {
+    public void FLUSH_white_wins_with_a_full_house_and_higher_cards() {
         assertWinner("2C 2S 2H AH AD", "3D 3S 3C TD TS", "White wins. - with full house and higher hand");
     }
 
     @Test
-    public void
-    FOUR_OF_A_KINDS_white_wins() {
+    public void FOUR_OF_A_KINDS_white_wins() {
         assertWinner("2C 3S 8H TH AD", "9D 9S 9C 9H TS", "White wins. - with four of a kind");
     }
 
     @Test
-    public void
-    FOUR_OF_A_KINDS_black_wins() {
-        assertWinner( "8D TS TC TH TD","2C 3S 8H TH AD", "Black wins. - with four of a kind");
+    public void FOUR_OF_A_KINDS_black_wins() {
+        assertWinner("8D TS TC TH TD", "2C 3S 8H TH AD", "Black wins. - with four of a kind");
     }
 
     @Test
-    public void
-    FOUR_OF_A_KINDS_black_wins_with_higher_cards() {
-        assertWinner( "8D TS TC TH TD","2C 4S 4H 4C 4D", "Black wins. - with four of a kind and higher hand");
+    public void FOUR_OF_A_KINDS_black_wins_with_higher_cards() {
+        assertWinner("8D TS TC TH TD", "2C 4S 4H 4C 4D", "Black wins. - with four of a kind and higher hand");
     }
-    
+
     @Test
-    public void
-    FOUR_OF_A_KINDS_white_wins_with_higher_cards() {
-        assertWinner( "8D TS TC TH TD","JC JS JH JD AD", "White wins. - with four of a kind and higher hand");
+    public void FOUR_OF_A_KINDS_white_wins_with_higher_cards() {
+        assertWinner("8D TS TC TH TD", "JC JS JH JD AD", "White wins. - with four of a kind and higher hand");
+    }
+
+    @Disabled
+    @Test
+    public void STRAIGHT_FLUSH_black_wins() {
+        assertWinner("8S 9S TS JS QS", "JC JS JH JD AD", "Black wins. - with straight flush");
+    }
+
+    @Disabled
+    @Test
+    public void STRAIGHT_FLUSH_white_wins() {
+        assertWinner("JC JS JH JD AD", "2H 3H 4H 5H 6H", "White wins. - with straight flush");
     }
 }
