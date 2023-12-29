9f3211383580e239c72a06974568b8e41a612e4e
aatwi <ahmadatwi23@gmail.com>
2023-12-29 13:16:59 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 38
        test: 0
    test-stats:
        run: 74
        passed: 74
        failed: 0
        skipped: 4
        error: 0
        duration: 82ms

--- java/src/main/java/com/murex/hands/FullHouseHand.java
+++ java/src/main/java/com/murex/hands/FullHouseHand.java
@@ -1,6 +1,15 @@
 package com.murex.hands;
 
+import com.murex.Card;
 import com.murex.Hand;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class FullHouseHand {
     private final Hand hand;
@@ -11,5 +20,14 @@ public class FullHouseHand {
 
     public Hand getHand() {
         return hand;
+    }
+
+    public boolean hasFullHouse() {
+        Hand hand = getHand();
+        Map<Card, Long> cardGroupsMap = Arrays.stream(hand.getCards()).collect(groupingBy(Function.identity(), counting()));
+        Optional<Card> pairCards = cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 2).findAny();
+        Optional<Card> trioCards = cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 3).findAny();
+
+        return pairCards.isPresent() && trioCards.isPresent();
     }
 }
--- java/src/main/java/com/murex/ranking/FullHouseRanking.java
+++ java/src/main/java/com/murex/ranking/FullHouseRanking.java
@@ -1,16 +1,9 @@
 package com.murex.ranking;
 
-import com.murex.Card;
 import com.murex.Hand;
 import com.murex.Result;
 import com.murex.hands.FullHouseHand;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
-
-import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class FullHouseRanking extends HandRanking{
@@ -26,22 +19,13 @@ public class FullHouseRanking extends HandRanking{
 
     @Override
     public Result getMatchingResult() {
-        if(hasFullHouse(blackFullHouseHand)) {
+        if(blackFullHouseHand.hasFullHouse()) {
             return Result.aMatchResult("Black wins. - with full house");
         }
 
-        if(hasFullHouse(whiteFullHouseHand)) {
+        if(whiteFullHouseHand.hasFullHouse()) {
             return Result.aMatchResult("White wins. - with full house");
         }
         return super.getMatchingResult();
-    }
-
-    public boolean hasFullHouse(FullHouseHand whiteFullHouseHand) {
-        Hand hand = whiteFullHouseHand.getHand();
-        Map<Card, Long> cardGroupsMap = Arrays.stream(hand.getCards()).collect(groupingBy(Function.identity(), counting()));
-        Optional<Card> pairCards = cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 2).findAny();
-        Optional<Card> trioCards = cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 3).findAny();
-
-        return pairCards.isPresent() && trioCards.isPresent();
     }
 }
