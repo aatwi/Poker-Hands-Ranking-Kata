e8e345858eb502f43f2382e82367e948c94197c9
aatwi <ahmadatwi23@gmail.com>
2023-12-29 16:11:41 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 1
    test-stats:
        run: 97
        passed: 97
        failed: 0
        skipped: 2
        error: 0
        duration: 73ms

--- java/src/test/java/com/murex/PokerGameTest.java
+++ java/src/test/java/com/murex/PokerGameTest.java
@@ -168,13 +168,12 @@ public class PokerGameTest {
     public void STRAIGHT_FLUSH_black_wins() {
         assertWinner("8S 9S TS JS QS", "JC JS JH JD AD", "Black wins. - with straight flush");
     }
-    
+
     @Test
     public void STRAIGHT_FLUSH_white_wins() {
         assertWinner("JC JS JH JD AD", "2H 3H 4H 5H 6H", "White wins. - with straight flush");
     }
-
-    @Disabled
+    
     @Test
     public void STRAIGHT_FLUSH_black_wins_and_higher_hand() {
         assertWinner("8D 9D TD JD QD", "2H 3H 4H 5H 6H", "Black wins. - with straight flush and higher hand");
