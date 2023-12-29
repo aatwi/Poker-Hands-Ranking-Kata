82aa2184d51a0b17070abc38a324a84560f4dc32
aatwi <ahmadatwi23@gmail.com>
2023-12-29 14:14:06 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 6
    test-stats:
        run: 79
        passed: 79
        failed: 0
        skipped: 1
        error: 0
        duration: 75ms

--- java/src/test/java/com/murex/ranking/FullHouseRankingTest.java
+++ java/src/test/java/com/murex/ranking/FullHouseRankingTest.java
@@ -68,7 +68,9 @@ class FullHouseRankingTest {
 
     @Test
     public void it_should_return_a_matching_result_result_when_white_and_black_have_full_house_and_higher_cards_for_black() {
-        FullHouseRanking fullHouseCardRanking = buildFullHouseCardRanking("5H 5S 5C TH TD", "4D 4S 4D JS JC");
-        assertEquals(aMatchResult("Black wins. - with full house and higher hand"), fullHouseCardRanking.getMatchingResult());
+        assertMatchingResult(
+                "5H 5S 5C TH TD",
+                "4D 4S 4D JS JC",
+                aMatchResult("Black wins. - with full house and higher hand"));
     }
 }
