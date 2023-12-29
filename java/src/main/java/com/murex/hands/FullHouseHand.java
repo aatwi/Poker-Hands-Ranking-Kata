c845c61ccdc8e268fbad1dcf7591270ed93785cb
aatwi <ahmadatwi23@gmail.com>
2023-12-29 13:16:38 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 29
        test: 0
    test-stats:
        run: 74
        passed: 74
        failed: 0
        skipped: 4
        error: 0
        duration: 79ms

---
+++ java/src/main/java/com/murex/hands/FullHouseHand.java
@@ -0,0 +1,15 @@
+package com.murex.hands;
+
+import com.murex.Hand;
+
+public class FullHouseHand {
+    private final Hand hand;
+
+    public FullHouseHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+}
--- java/src/main/java/com/murex/ranking/FullHouseRanking.java
+++ java/src/main/java/com/murex/ranking/FullHouseRanking.java
@@ -3,6 +3,7 @@ package com.murex.ranking;
 import com.murex.Card;
 import com.murex.Hand;
 import com.murex.Result;
+import com.murex.hands.FullHouseHand;
 
 import java.util.Arrays;
 import java.util.Map;
@@ -13,23 +14,30 @@ import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class FullHouseRanking extends HandRanking{
+
+    private final FullHouseHand blackFullHouseHand;
+    private final FullHouseHand whiteFullHouseHand;
+
     public FullHouseRanking(Hand blackHand, Hand whiteHand) {
         super(blackHand, whiteHand);
+        blackFullHouseHand = new FullHouseHand(blackHand);
+        whiteFullHouseHand = new FullHouseHand(whiteHand);
     }
 
     @Override
     public Result getMatchingResult() {
-        if(hasFullHouse(blackHand)) {
+        if(hasFullHouse(blackFullHouseHand)) {
             return Result.aMatchResult("Black wins. - with full house");
         }
 
-        if(hasFullHouse(whiteHand)) {
+        if(hasFullHouse(whiteFullHouseHand)) {
             return Result.aMatchResult("White wins. - with full house");
         }
         return super.getMatchingResult();
     }
 
-    private boolean hasFullHouse(Hand hand) {
+    public boolean hasFullHouse(FullHouseHand whiteFullHouseHand) {
+        Hand hand = whiteFullHouseHand.getHand();
         Map<Card, Long> cardGroupsMap = Arrays.stream(hand.getCards()).collect(groupingBy(Function.identity(), counting()));
         Optional<Card> pairCards = cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 2).findAny();
         Optional<Card> trioCards = cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 3).findAny();
