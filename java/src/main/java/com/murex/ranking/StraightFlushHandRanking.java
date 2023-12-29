396fa97c8069e0ea7ceaf2a261cebbe81580bad6
aatwi <ahmadatwi23@gmail.com>
2023-12-29 15:48:19 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 7
        test: 13
    test-stats:
        run: 90
        passed: 90
        failed: 0
        skipped: 5
        error: 0
        duration: 67ms

--- java/src/main/java/com/murex/ranking/StraightFlushHandRanking.java
+++ java/src/main/java/com/murex/ranking/StraightFlushHandRanking.java
@@ -2,6 +2,8 @@ package com.murex.ranking;
 
 import com.murex.Hand;
 import com.murex.Result;
+import com.murex.hands.FlushHand;
+import com.murex.hands.StraightHand;
 
 public class StraightFlushHandRanking extends HandRanking{
     public StraightFlushHandRanking(Hand blackHand, Hand whiteHand) {
@@ -10,6 +12,11 @@ public class StraightFlushHandRanking extends HandRanking{
 
     @Override
     public Result getMatchingResult() {
+        StraightHand straightHand = new StraightHand(whiteHand);
+        FlushHand flushHand = new FlushHand(whiteHand);
+        if(straightHand.isStraight() && flushHand.isFlush()) {
+            return Result.aMatchResult("White wins. - with straight flush");
+        }
         return super.getMatchingResult();
     }
 }
--- java/src/test/java/com/murex/ranking/StraightFlushHandRankingTest.java
+++ java/src/test/java/com/murex/ranking/StraightFlushHandRankingTest.java
@@ -3,6 +3,7 @@ package com.murex.ranking;
 import com.murex.Hand;
 import org.junit.jupiter.api.Test;
 
+import static com.murex.Result.aMatchResult;
 import static com.murex.Result.aNoMatchResult;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +14,10 @@ class StraightFlushHandRankingTest {
         assertEquals(aNoMatchResult(), handRanking.getMatchingResult());
     }
 
+    private static void assertMatchingResult(String blackCards, String whiteCards, String message) {
+        StraightFlushHandRanking handRanking = buildStraightFlushHandRanking(blackCards, whiteCards);
+        assertEquals(aMatchResult(message), handRanking.getMatchingResult());
+    }
     private static StraightFlushHandRanking buildStraightFlushHandRanking(String blackCards, String whiteCards) {
         Hand blackHand = Hand.buildFrom("Black", blackCards);
         Hand whiteHand = Hand.buildFrom("White", whiteCards);
@@ -23,5 +28,13 @@ class StraightFlushHandRankingTest {
     @Test
     public void it_should_return_a_no_matching_result_when_non_of_the_players_have_straight_flush_cards() {
         assertNonMatchingResult("7H 8C TD KH AS", "2D 3H 5C 9S KH");
+    }
+
+    @Test
+    public void it_should_return_a_matching_result_with_white_as_winner_having_straight_cards_2_to_6() {
+        assertMatchingResult(
+                "7H 8C TD KH AS",
+                "2D 3D 4D 5D 6D",
+                "White wins. - with straight flush");
     }
 }
