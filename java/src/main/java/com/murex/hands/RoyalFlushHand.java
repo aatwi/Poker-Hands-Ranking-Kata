ed356553ce8172ad5e289406c28bdb7d14749700
aatwi <ahmadatwi23@gmail.com>
2023-12-29 16:33:31 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 24
        test: 0
    test-stats:
        run: 101
        passed: 101
        failed: 0
        skipped: 4
        error: 0
        duration: 69ms

--- java/src/main/java/com/murex/hands/RoyalFlushHand.java
+++ java/src/main/java/com/murex/hands/RoyalFlushHand.java
@@ -4,12 +4,21 @@ import com.murex.Hand;
 
 public class RoyalFlushHand {
     private final Hand hand;
+    private final StraightFlushHand straightFlushHand;
 
     public RoyalFlushHand(Hand hand) {
         this.hand = hand;
+        straightFlushHand = new StraightFlushHand(hand);
     }
 
     public Hand getHand() {
         return hand;
+    }
+
+    public boolean isRoyalFlush() {
+        if(this.straightFlushHand.isStraightFlush()){
+            return hand.getCardAt(4).getCharValue() == 'A';
+        }
+        return false;
     }
 }
--- java/src/main/java/com/murex/ranking/RoyalFlushHandRanking.java
+++ java/src/main/java/com/murex/ranking/RoyalFlushHandRanking.java
@@ -3,7 +3,6 @@ package com.murex.ranking;
 import com.murex.Hand;
 import com.murex.Result;
 import com.murex.hands.RoyalFlushHand;
-import com.murex.hands.StraightFlushHand;
 
 public class RoyalFlushHandRanking extends HandRanking{
 
@@ -18,24 +17,14 @@ public class RoyalFlushHandRanking extends HandRanking{
 
     @Override
     public Result getMatchingResult() {
-        if (isRoyalFlush(whiteRoyalFlushHand)) {
+        if (whiteRoyalFlushHand.isRoyalFlush()) {
             return Result.aMatchResult("White wins. - with royal flush");
         }
-        if (isRoyalFlush(blackRoyalFlushHand)) {
+        if (blackRoyalFlushHand.isRoyalFlush()) {
             return Result.aMatchResult("Black wins. - with royal flush");
         }
         return super.getMatchingResult();
     }
 
-    private boolean isRoyalFlush(RoyalFlushHand royalFlushHand) {
-        Hand hand = royalFlushHand.getHand();
-        StraightFlushHand white = new StraightFlushHand(hand);
-        if(white.isStraightFlush()){
-            if (white.getHand().getCardAt(4).getCharValue() == 'A') {
-                return true;
-            }
-        }
-        return false;
-    }
 }
  
