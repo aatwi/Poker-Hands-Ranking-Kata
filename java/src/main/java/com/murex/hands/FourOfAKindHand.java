59b7d4afb2ea926a89465913deabeac4a7a86ec9
aatwi <ahmadatwi23@gmail.com>
2023-12-29 15:15:57 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 34
        test: 0
    test-stats:
        run: 82
        passed: 82
        failed: 0
        skipped: 5
        error: 0
        duration: 75ms

--- java/src/main/java/com/murex/hands/FourOfAKindHand.java
+++ java/src/main/java/com/murex/hands/FourOfAKindHand.java
@@ -1,6 +1,14 @@
 package com.murex.hands;
 
+import com.murex.Card;
 import com.murex.Hand;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class FourOfAKindHand {
     private final Hand hand;
@@ -11,5 +19,10 @@ public class FourOfAKindHand {
 
     public Hand getHand() {
         return hand;
+    }
+
+    public boolean hasFourOfAKind() {
+        Map<Card, Long> cardGroupsMap = Arrays.stream(getHand().getCards()).collect(groupingBy(Function.identity(), counting()));
+        return cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 4).findAny().isPresent();
     }
 }
--- java/src/main/java/com/murex/ranking/FourOfAKindRanking.java
+++ java/src/main/java/com/murex/ranking/FourOfAKindRanking.java
@@ -1,39 +1,30 @@
 package com.murex.ranking;
 
-import com.murex.Card;
 import com.murex.Hand;
 import com.murex.Result;
 import com.murex.hands.FourOfAKindHand;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.function.Function;
-
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
 
 public class FourOfAKindRanking extends HandRanking {
 
-    private final FourOfAKindHand fourOfAKindHand;
+    private final FourOfAKindHand whiteFourOfAKindHand;
+    private final FourOfAKindHand blackFourOfAKindHand;
 
     public FourOfAKindRanking(Hand blackHand, Hand whiteHand) {
         super(blackHand, whiteHand);
-        fourOfAKindHand = new FourOfAKindHand(whiteHand);
+        blackFourOfAKindHand = new FourOfAKindHand(blackHand);
+        whiteFourOfAKindHand = new FourOfAKindHand(whiteHand);
     }
 
     @Override
     public Result getMatchingResult() {
-        if (hasFourOfAKind(blackHand)) {
+        if (blackFourOfAKindHand.hasFourOfAKind()) {
             return Result.aMatchResult("Black wins. - with four of a kind");
         }
-        if (hasFourOfAKind(whiteHand)) {
+        if (whiteFourOfAKindHand.hasFourOfAKind()) {
             return Result.aMatchResult("White wins. - with four of a kind");
         }
         return super.getMatchingResult();
     }
 
-    private boolean hasFourOfAKind(Hand hand) {
-        Map<Card, Long> cardGroupsMap = Arrays.stream(hand.getCards()).collect(groupingBy(Function.identity(), counting()));
-        return cardGroupsMap.keySet().stream().filter(card -> cardGroupsMap.get(card) == 4).findAny().isPresent();
-    }
 }
