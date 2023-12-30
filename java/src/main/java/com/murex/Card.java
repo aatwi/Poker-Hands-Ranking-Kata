fd373371d0d6ebd28016df8c4cff0b77ee8ecd21
aatwi <ahmadatwi23@gmail.com>
2023-12-30 16:57:32 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 5
        test: 0
    test-stats:
        run: 114
        passed: 114
        failed: 0
        skipped: 1
        error: 0
        duration: 97ms

--- java/src/main/java/com/murex/Card.java
+++ java/src/main/java/com/murex/Card.java
@@ -33,10 +33,7 @@ public class Card implements Comparable<Card> {
 
     @Override
     public int compareTo(Card otherCard) {
-        int otherValue = otherCard.getIntValue();
-        int currentValue = cardNumber.getIntValue();
-
-        return Integer.compare(currentValue, otherValue);
+        return Integer.compare(cardNumber.getIntValue(), otherCard.getIntValue());
     }
 
     @Override
